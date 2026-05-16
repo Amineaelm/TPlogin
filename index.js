2e5f18740d5578577ca2bd7b2ab910e5841cd054
Amineaelm <amine.elmousghi@gmail.com>
2026-05-16 16:10:00 +0100

    login+

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ button.addEventListener("click",()=>{
     const nomValue=nom.value;
     const emailValue=email.value;
     const passwordValue=password.value;
-    if(!nomValue || !emailValue || !passwordValue){
+    if(!nomValue && !emailValue && !passwordValue){
+
         alert("Please fill in all fields.");
-    } else {
+    } else if(!nomValue){
+        alert("Please enter your name.");
+    } else if(!emailValue){
+        alert("Please enter your email.");
+    }else if(!passwordValue){
+        alert("Please enter your password.");
+    }
+    else {
         alert("Name:"+nomValue+"\nEmail:"+emailValue+"\nPassword:"+passwordValue);
     }});
